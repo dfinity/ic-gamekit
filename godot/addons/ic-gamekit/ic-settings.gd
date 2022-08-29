0dd28a4915dc43fe43072542821dcce3edb623b3
Vincent Zhan <zhangwei983@hotmail.com>
2022-08-29 16:19:00 +0800

    Refactor the save/load settings code

--- godot/addons/ic-gamekit/ic-settings.gd
+++ godot/addons/ic-gamekit/ic-settings.gd
@@ -4,47 +4,53 @@ extends Control
 const settings_path = "res://addons/ic-gamekit/ic-settings.json"
 const default_canister_name = "godot_html5_assets"
 
-var settings_data = { }
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	load_settings()
+	init_settings()
 
-func _on_SaveSettingsBtn_pressed():
-	settings_data["CanisterName"] = $HBoxContainer/RightColumn/CanisterNameInput.text;
-	settings_data["ICConnectorEnabled"] = $HBoxContainer/RightColumn/EnableCheckBox.pressed;
-	save_settings(settings_data);
+func init_settings():
+	var settings_data = load_settings()
+	$HBoxContainer/RightColumn/CanisterNameInput.text = settings_data["CanisterName"]
+	$HBoxContainer/RightColumn/EnableCheckBox.pressed = settings_data["ICConnectorEnabled"]
+	
+	print("IC Settings initialized.")
 
 func load_settings():
+	var settings_data = { }
+	
+	# Load the settings from the json file if it exists.
 	var file = File.new()
-	
-	if not file.file_exists(settings_path):
-		$HBoxContainer/RightColumn/CanisterNameInput.text = default_canister_name
-		$HBoxContainer/RightColumn/EnableCheckBox.pressed = false
-		return
+	if file.file_exists(settings_path):
+		file.open(settings_path, file.READ)
 		
-	file.open(settings_path, file.READ)
-	
-	var text = file.get_as_text();
-	settings_data = parse_json(text)
-	file.close()
-	
-	if settings_data.has("CanisterName") :
-		$HBoxContainer/RightColumn/CanisterNameInput.text = settings_data["CanisterName"]
-	else:
-		$HBoxContainer/RightColumn/CanisterNameInput.text = default_canister_name
+		var text = file.get_as_text();
+		settings_data = parse_json(text)
 		
-	if settings_data.has("ICConnectorEnabled") :
-		$HBoxContainer/RightColumn/EnableCheckBox.pressed = settings_data["ICConnectorEnabled"]
-	else:
-		$HBoxContainer/RightColumn/EnableCheckBox.pressed = false
+		file.close()
 	
-	print("IC Settings loaded.")
+	# Make sure the necessary values exist in the dictionary.
+	if not settings_data.has("CanisterName") :
+		settings_data["CanisterName"] = default_canister_name
 	
-func save_settings(var data):
+	if not settings_data.has("ICConnectorEnabled") :
+		settings_data["ICConnectorEnabled"] = false
+		
+	return settings_data
+
+func _on_SaveSettingsBtn_pressed():
+	save_settings();
+
+func save_settings():
+	# Prepare the settings data.
+	var settings_data = {
+		"CanisterName" : $HBoxContainer/RightColumn/CanisterNameInput.text,
+		"ICConnectorEnabled" : $HBoxContainer/RightColumn/EnableCheckBox.pressed
+	}
+	
+	# Save the settings data as json file.
 	var file = File.new();
 	file.open(settings_path, File.WRITE)
-	var json_string = JSON.print(data, "  ");
+	var json_string = JSON.print(settings_data, "  ");
 	file.store_string(json_string)
 	file.close()
 	
