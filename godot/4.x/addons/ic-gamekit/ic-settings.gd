f780fd0bf5cc63f8fc378cbdefb208dd889eb233
Vincent Zhang <zhangwei983@hotmail.com>
2022-10-26 12:12:08 +0800

    Change to load script rather than using global script.

--- godot/4.x/addons/ic-gamekit/ic-settings.gd
+++ godot/4.x/addons/ic-gamekit/ic-settings.gd
@@ -3,14 +3,16 @@ extends Control
 
 const settings_path = "res://addons/ic-gamekit/ic-settings.json"
 
+var utilities_script
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	utilities_script = load("res://addons/ic-gamekit/ic-settings-utilities.gd")
 	init_settings()
 
 
 func init_settings():
-	var settings_data = ICSettingsUtilities.load_settings(settings_path)
+	var settings_data = utilities_script.load_settings(settings_path)
 	$HBoxContainer/RightColumn/CanisterNameInput.text = settings_data["CanisterName"]
 	$HBoxContainer/RightColumn/EnableCheckBox.button_pressed = settings_data["ICConnectorEnabled"]
 	
@@ -26,6 +28,6 @@ func save_settings():
 		"CanisterName" : $HBoxContainer/RightColumn/CanisterNameInput.text,
 		"ICConnectorEnabled" : $HBoxContainer/RightColumn/EnableCheckBox.pressed
 	}
-	ICSettingsUtilities.save_settings(settings_path, settings_data)
+	utilities_script.save_settings(settings_path, settings_data)
 	
 	print("IC Settings saved.")
