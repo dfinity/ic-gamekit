ab5156893f7940cae835034db78b65ba4469a149
Vincent Zhang <vincent.zhang@dfinity.org>
2023-08-01 14:27:58 +0800

    Revert "Update to the latest ICP.NET at fe3335bc3e4012bb97ba2f06ef3921d3155bf059"
    This reverts commit d8c79ccfd7a8cebe251213f97be00f7521c16833.

--- examples/native_apps/unity_android_deeplink/android_integration/Assets/Scripts/PluginProxy.cs
+++ examples/native_apps/unity_android_deeplink/android_integration/Assets/Scripts/PluginProxy.cs
@@ -27,7 +27,7 @@ namespace IC.GameKit
 
         public void OpenBrowser()
         {
-            var target = testTarget + "?sessionkey=" + ByteUtil.ToHexString(mTestICPAgent.TestIdentity.PublicKey.PublicKey);
+            var target = testTarget + "?sessionkey=" + ByteUtil.ToHexString(mTestICPAgent.TestIdentity.PublicKey.Value);
 
 #if UNITY_ANDROID
             mPlugin.Call("openBrowser", target);
